00edc5e2fdbd43a8fb927b1babaa32cc75c5d2e4
Wonder Diaz <wonderdxk@gmail.com>
2020-08-06 23:58:52 -0400

    1.0.10
    Modal Confirm Action
    Merge Component Name
    Save document with Code Customer

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,33 @@
 <template>
   <main class="container flex items-center flex-wrap w-full my-0 mx-auto overflow-hidden">
     <form class="flex flex-wrap justify-center py-4 w-full max-w-8xl my-0 mx-auto h-full">
-      <Customers />
+      <section class="flex flex-wrap w-full px-3 my-2 lg:my-3">
+        <h1
+          v-if="customerName.length > 0"
+          class="block w-full text-gray-900 text-center text-lg bold py-2 uppercase"
+        >
+          Bienvenido!,
+          <span class="block w-full">{{ customerName }}</span>
+        </h1>
+        <label
+          class="block uppercase tracking-wide text-gray-900 text-xs font-bold mb-2"
+          for="grid-code"
+        >Ingrese su nombre de cliente</label>
+        <v-select
+          class="block appearance-none w-full bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-4"
+          v-model="customer"
+          :options="paginated"
+          @search="query => (search = query)"
+          :filterable="false"
+          label="name"
+          :value="paginated.name"
+          placeholder="NOMBRE DEL CLIENTE"
+        ></v-select>
+        <button
+          class="block bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded cursor-pointer uppercase text-sm my-0 mx-auto"
+          @click.prevent="addCustomer"
+        >Guardar cliente</button>
+      </section>
       <section class="w-full lg:w-1/2 px-3 my-2 lg:my-3">
         <label
           class="block uppercase tracking-wide text-gray-900 text-xs font-bold mb-2"
@@ -110,13 +136,14 @@
       </table>
       <button
         class="bg-green-500 hover:bg-green-700 text-white my-4 py-2 px-4 rounded cursor-pointer uppercase text-sm"
-        @click.prevent="saveArchivetxt"
+        @click.prevent="saveArchive"
       >Guardar archivo</button>
     </section>
   </main>
 </template>
 
 <script>
+import customersDataJson from "../data/customers.json";
 import productsDataJson from "../data/products.json";
 import { saveAs } from "file-saver";
 const FileSaver = require("file-saver");
@@ -133,8 +160,13 @@ export default {
       collections: 0, //Precio Total
       productsData: Object.values(productsDataJson), //Data de productos
       file: null, //
-      customerName: "",
-      customerCode: "",
+      customer: [], //Cliente
+      customerName: "", //Cliente Seleccionado
+      customerCode: "", //Cliente guardado
+      customersData: Object.values(customersDataJson), //Data de clientes
+      search: "",
+      offset: 0,
+      limit: 3, //Limite de clientes visibles
     };
   },
   mounted() {
@@ -158,12 +190,19 @@ export default {
     if (localStorage.getItem("customerName")) {
       this.customerName = JSON.parse(localStorage.getItem("customerName"));
     }
-    // Guarda el codigo del cliente
-    if (localStorage.getItem("customerCode")) {
-      this.customerCode = JSON.parse(localStorage.getItem("customerCode"));
-    }
   },
   methods: {
+    addCustomer() {
+      (this.customerCode = this.customer.code),
+        (this.customerName = this.customer.name);
+      this.saveCustomer();
+    },
+    saveCustomer() {
+      const customerName = JSON.stringify(this.customerName);
+      localStorage.setItem("customerName", customerName);
+      const customerCode = JSON.stringify(this.customerCode);
+      localStorage.setItem("customerCode", customerCode);
+    },
     addQuantity() {
       if (!this.productSelected.price) {
         return;
@@ -177,12 +216,12 @@ export default {
       if (!this.quantity) {
         return;
       }
-      //localStorage.customerCurrent = this.customer;
       this.totalPrice += this.productSelected.price * this.quantity;
       this.collections = this.totalPrice / 50;
       this.allProducts.push(
         (this.productsSelected = [
-          "\n", this.productSelected.name, //Nombre
+          "\n",
+          this.productSelected.name, //Nombre
           this.productSelected.code, //Codigo
           parseFloat(this.quantity), //Cantidad
           this.productSelected.price, //Precio Unitario
@@ -203,32 +242,64 @@ export default {
       localStorage.setItem("collections", collections);
     },
     removeEach(x) {
-      this.totalPrice -= this.allProducts[x][4];
+      this.totalPrice -= this.allProducts[x][5];
       this.collections = this.totalPrice / 50;
       this.allProducts.splice(x, 1);
       this.saveAll();
     },
-    saveArchivetxt() {
-      this.file = new File(
-        [
-          "Cliente:\n" + this.customerName,
-          "\nCodigo:\n" + this.customerCode,
-          "\nProductos:" + this.allProducts,
-          "\nPrecio Total:\n" + this.totalPrice,
-          "\nColecciones:\n" + this.collections,
-        ],
-        "Pedido.csv",
-        { type: "data:text/csv;charset=utf-8" }
-      );
-      FileSaver.saveAs(this.file);
+    saveArchive(x) {
+      if (
+        confirm("Ok para Guardar y borrar \n Cancelar para Guarda sin borrar")
+      ) {
+        this.file = new File(
+          [
+            "Cliente:\n" + this.customerName,
+            "\nCodigo:\n" + this.customerCode,
+            "\nProductos:" + this.allProducts,
+            "\nPrecio Total:\n" + this.totalPrice,
+            "\nColecciones:\n" + this.collections,
+          ],
+          "Pedido.csv",
+          { type: "data:text/csv;charset=utf-8" }
+        );
+        FileSaver.saveAs(this.file);
+        //Borrar Visual
+        this.allProducts.splice(x);
+        //Borrar LocalStorage
+        localStorage.clear();
+        //Restablecer Valores
+        this.totalPrice = 0;
+        this.collections= 0;
+        this.quantity = null;
+        //Salvar los valores reestrablecidos
+        this.saveAll();
+      } else {
+        this.file = new File(
+          [
+            "Cliente:\n" + this.customerName,
+            "\nCodigo:\n" + this.customerCode,
+            "\nProductos:" + this.allProducts,
+            "\nPrecio Total:\n" + this.totalPrice,
+            "\nColecciones:\n" + this.collections,
+          ],
+          "Pedido de " + this.customerCode + ".csv",
+          { type: "data:text/csv;charset=utf-8" }
+        );
+        FileSaver.saveAs(this.file);
+      }
     },
   },
   computed: {
-
+    filtered() {
+      return this.customersData.filter((customer) =>
+        customer.name.match(new RegExp(this.search, "gi"))
+      );
+    },
+    paginated() {
+      return this.filtered.slice(this.offset, this.limit + this.offset);
+    },
   },
-  watch: {
-
-  },
+  watch: {},
 };
 </script>
 
